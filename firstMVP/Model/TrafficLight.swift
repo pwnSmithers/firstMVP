5cff3edd77290c77dc0033cd4a4702b29a085d7d
Smith <smith.mugalu@andela.com>
2019-10-08 12:59:46 +0300

    setup struct

---
+++ firstMVP/Model/TrafficLight.swift
@@ -0,0 +1,14 @@
+//
+//  TrafficLight.swift
+//  firstMVP
+//
+//  Created by Smithers on 08/10/2019.
+//  Copyright © 2019 Smithers. All rights reserved.
+//
+
+import Foundation
+
+struct TrafficLight {
+    let colorName: String
+    let description: String
+}
